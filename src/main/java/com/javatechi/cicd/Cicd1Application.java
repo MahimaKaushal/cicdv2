14b01d642e3c9be7eda541cd74bf90df9debbfbb
Mahima Kaushal <30713765+MahimaKaushal@users.noreply.github.com>
2022-03-28 23:56:45 +0530

    updated controller

--- src/main/java/com/javatechi/cicd/Cicd1Application.java
+++ src/main/java/com/javatechi/cicd/Cicd1Application.java
@@ -3,11 +3,13 @@ package com.javatechi.cicd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/hello")
 @SpringBootApplication
 public class Cicd1Application {
 
-	@GetMapping
+	@GetMapping("/cicd")
 	public String hl() {
 		return "hi cicd application";
 	}
